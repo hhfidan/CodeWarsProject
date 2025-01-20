132c4d4e277537838e6335f038819f0b30261fc7
hhfidan <68022458+hhfidan@users.noreply.github.com>
2025-01-20 12:48:47 +0300

    Create Array.diff.py

---
+++ Array.diff.py
@@ -0,0 +1,7 @@
+# problem link: https://www.codewars.com/kata/523f5d21c841566fde000009
+
+# you can find my solution(list comp.) below;
+
+def array_diff(a, b):
+
+    return [x for x in a if x not in b]
